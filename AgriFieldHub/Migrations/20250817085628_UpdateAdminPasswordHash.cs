93e899f95025cd89913cccdf340c974fbea4e8a6
Your Name <you@example.com>
2025-08-17 11:56:54 +0300

    chore(security): update seeded admin password hash

---
+++ AgriFieldHub/Migrations/20250817085628_UpdateAdminPasswordHash.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+
+#nullable disable
+
+namespace AgriFieldHub.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateAdminPasswordHash : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "PasswordHash" },
+                values: new object[] { "p9pY0i4lT3b2kWmQp7sQ2w==:b4W7k4j7nZ8R5kQ1l6k+o7uXv6gJ2x5tqS3Tg8yG5lI=" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "PasswordHash" },
+                values: new object[] { "hashed" });
+        }
+    }
+}
